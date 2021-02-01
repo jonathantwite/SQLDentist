c7d9f11b63a6377bf2306bc8421477a40e3c70fb
Jonathan Twite <JonathanTwite@democracycounts.co.uk>
2021-02-01 17:29:18 +0000

    feat(Favicon): Initial favicon creation

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,9 +11,16 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
+            { hid: 'description', name: 'description', content: '' },
+            { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#00aba9' },
+            { hid: 'theme-color', name: 'theme-color', content: '#ffffff' }
         ],
         link: [
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
