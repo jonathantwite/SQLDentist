761b3bec2bf0098e28bad18f0b5a0462bdbf0d36
Jonathan Twite <JonathanTwite@democracycounts.co.uk>
2021-02-19 10:39:29 +0000

    feat(Code): show SQL version on code

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -26,8 +26,27 @@ export default {
     },
     mounted() {
         document.getElementsByClassName('language-text')
-            .forEach(e=>e.classList.remove('line-numbers'));
+            .forEach(e => e.classList.remove('line-numbers'));
+
         Prism.highlightAll();
+
+        document.getElementsByClassName('filename')
+            .forEach((e) => {
+                const split = e.textContent.split('//');
+                const filename = split[0];
+                const version = split[1];
+
+                if (version) {
+                    e.textContent = '';
+                    e.appendChild(document.createTextNode(filename));
+
+                    const versEl = document.createElement('em');
+                    versEl.classList.add('d-block', 'text-right');
+                    versEl.appendChild(document.createTextNode('SQL Server ' + version));
+
+                    e.appendChild(versEl);
+                }
+            });
     }
 };
 </script>
