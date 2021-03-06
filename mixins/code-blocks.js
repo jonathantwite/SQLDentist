8b869ee24048f278ba19a0d62bcc80635637e20e
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2021-03-06 22:16:33 +0000

    refactor(code): Code block mixin

---
+++ mixins/code-blocks.js
@@ -0,0 +1,31 @@
+import Prism from 'prismjs';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
+import 'prismjs/components/prism-sql.js';
+export default {
+    mounted() {
+        document.getElementsByClassName('language-text')
+            .forEach(e => e.classList.remove('line-numbers'));
+
+        Prism.highlightAll();
+
+        document.getElementsByClassName('filename')
+            .forEach((e) => {
+                const split = e.textContent.split('//');
+                const filename = split[0];
+                const version = split[1];
+
+                if (version) {
+                    e.textContent = '';
+                    e.appendChild(document.createTextNode(filename));
+
+                    const versEl = document.createElement('em');
+                    versEl.classList.add('d-block', 'text-right');
+                    versEl.appendChild(document.createTextNode('SQL Server ' + version));
+
+                    e.appendChild(versEl);
+                }
+            });
+    }
+};
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -9,12 +9,9 @@
 
 <script>
 import dayjs from 'dayjs';
-import Prism from 'prismjs';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
-import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
-import 'prismjs/components/prism-sql.js';
+import codeBlock from '@/mixins/code-blocks';
 export default {
+    mixins: [codeBlock],
     async asyncData({ $content, params }) {
         const page = await $content(`blog/${params.slug}` || 'blog/index').fetch();
         return { page };
@@ -23,30 +20,6 @@ export default {
         createdAtDisplay() {
             return dayjs(this.page.createdAt).format('dddd D MMMM YYYY');
         }
-    },
-    mounted() {
-        document.getElementsByClassName('language-text')
-            .forEach(e => e.classList.remove('line-numbers'));
-
-        Prism.highlightAll();
-
-        document.getElementsByClassName('filename')
-            .forEach((e) => {
-                const split = e.textContent.split('//');
-                const filename = split[0];
-                const version = split[1];
-
-                if (version) {
-                    e.textContent = '';
-                    e.appendChild(document.createTextNode(filename));
-
-                    const versEl = document.createElement('em');
-                    versEl.classList.add('d-block', 'text-right');
-                    versEl.appendChild(document.createTextNode('SQL Server ' + version));
-
-                    e.appendChild(versEl);
-                }
-            });
     }
 };
 </script>
