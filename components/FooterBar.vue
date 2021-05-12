eae1b6923064342da7cea35d3512446c89279b97
Jonathan Twite <13119611+jonathantwite@users.noreply.github.com>
2021-05-12 09:36:46 +0100

    fix(FooterBar): Increase contrast on copyright text

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer blog-footer py-3">
         <b-container>
-            <span class="text-muted">Copyright &copy; 2020-{{ currentYear }},&nbsp;&nbsp;Jonathan Twite</span>
+            <span>Copyright &copy; 2020-{{ currentYear }},&nbsp;&nbsp;Jonathan Twite</span>
         </b-container>
     </footer>
 </template>
